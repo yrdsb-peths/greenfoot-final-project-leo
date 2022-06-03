54fe774556216bef96466a569e1f61c7e8fed964
Xu <348962257@EDU.YRDSB.NET>
2022-06-03 10:16:52 -0400

    error

---
+++ Boundary.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Boundary here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boundary extends Actor
+{
+    /**
+     * Act - do whatever the Boundary wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    GreenfootImage bound = getImage();
+    public void Boundary()
+    {
+        // Add your action code here.
+        bound.scale(1, 1);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -20,6 +20,8 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         setBackground(new GreenfootImage("SMB.png"));
+        Boundary bound1 = new Boundary();
+        addObject(bound1, 100, 100);
         
         Elephant ele = new Elephant();
         addObject(ele, 200, 200);
